d6848b6df2dac0314c6131b798418d615d6d2ae2
kekuso <kekuso808@gmail.com>
2016-07-13 13:36:23 -1000

    Created various get, post, and put methods. tests ok.

---
+++ Gallery.js
@@ -0,0 +1,21 @@
+
+var path = require('path');
+var fs = require('fs');
+module.exports = {
+  create: addGallery
+};
+
+var JSON_DATA_PATH = path.resolve('data', 'gallery.json');
+
+function addGallery (data, callback) {
+  fs.readFile(JSON_DATA_PATH, 'utf8', function (err, json) {
+    if(err) {
+      throw err;
+    }
+    var galleries = JSON.parse(json);
+    galleries.push(data);
+    fs.writeFile(JSON_DATA_PATH, JSON.stringify(galleries), function(err) {
+      callback(err,data);
+    });
+  });
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+var pug = require('pug');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var querystring = require('querystring');
+var Gallery = require('./Gallery');
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+var visitorCount = 0;
+app.get('/', function (req, res) {
+  res.render('index', {visitorCount : ++visitorCount});
+  //res.send("Returning a list of gallery photos.");
+});
+
+app.get('/gallery/:id', function (req, res) {
+  if(req.params.id === 'new') {
+    res.send("Gallery submission form");
+  }
+  else {
+    res.send("Single gallery " + req.params.id);
+  }
+});
+
+app.post('/gallery', function (req, res) {
+    var locals = req.body;
+
+    Gallery.create(locals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log(result);
+      res.render('gallery', result);
+    });
+  //res.send("Creating a gallery with " + req.body.author + ", " + req.body.url + ", " + req.body.description);
+});
+
+app.put('/gallery/:id', function (req, res) {
+  var locals = req.body;
+  res.render('gallery', locals);
+});
+
+app.delete('/gallery/:id', function (req, res) {
+  res.send("Deleting gallery " + req.params.id);
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("App listening on http://%s:%s", host, port);
+});
