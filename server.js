1812e3830940cbab6bea96ca888e4651e559e085
kekuso <kekuso808@gmail.com>
2016-07-28 20:53:59 -1000

    Made separate scss file for gallery pages. added success and 404 images.

--- server.js
+++ server.js
@@ -148,7 +148,6 @@ app.delete('/gallery/:id', function (req, res) {
 
 var server = app.listen(3000, function () {
   db.sequelize.sync();
-
   var host = server.address().address;
   var port = server.address().port;
   console.log("App listening on http://%s:%s", host, port);
