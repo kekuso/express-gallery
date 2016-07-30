38ab116f732ce38718ef143825154768b4ddcebb
kekuso <kekuso808@gmail.com>
2016-07-30 11:05:10 -1000

    Added Basic Authentication with passportsjs

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 var pug = require('pug');
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var querystring = require('querystring');
 var Gallery = require('./Gallery');
 var methodOverride = require('method-override');
 var db = require('./models');
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
 
+var app = express();
 app.set('views', path.resolve(__dirname, 'views'));
 app.use(express.static('public'));
 app.set('view engine', 'pug');
-
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 var Picture = db.Picture;
+
+var user = { username: 'bob', password: 'secret4', email: 'bob@example.com' };
+passport.use(new BasicStrategy(
+  function(username, password, done) {
+    // Example authentication strategy using
+    if ( !(username === user.username && password === user.password) ) {
+      return done(null, false);
+    }
+    return done(null, user);
+}));
 
 app.get('/', function (req, res) {
   var locals = req.body;
@@ -26,39 +37,41 @@ app.get('/', function (req, res) {
     });
 });
 
+app.get('/gallery/new', passport.authenticate('basic', { session: false }),
+    function (req, res) {
+      res.render('newPhoto');
+    });
+
 app.get('/gallery/:id', function (req, res) {
-  if(req.params.id === 'new') {
-    res.render('newPhoto');
-  }
-  else {
-    var picId = parseInt(req.params.id);
-    Picture.findOne({
+  var picId = parseInt(req.params.id);
+  Picture.findOne({
+    where: {
+      id: picId
+    }
+  }).then(function (picture) {
+    if(!picture) {
+      console.log("Client tried accessing a picture that doesn't exist.");
+      res.render('404');
+    }
+    Picture.findAll({
       where: {
-        id: picId
+        id: {
+          $between: [picId, (picId + 3)]
+        }
       }
-    }).then(function (picture) {
-      if(!picture) {
-        console.log("Client tried accessing a picture that doesn't exist.");
+    }).then(function (pictures) {
+      if(pictures.length > 0) {
+        res.render('gallery', {json: pictures, id: parseInt(req.params.id)});
+      }
+      else {
+        console.log("No pictures to display to client.");
         res.render('404');
       }
-      Picture.findAll({
-        where: {
-          id: {
-            $between: [picId, (picId + 3)]
-          }
-        }
-      }).then(function (pictures) {
-        if(pictures.length > 0) {
-          res.render('gallery', {json: pictures, id: parseInt(req.params.id)});
-        }
-        else {
-          console.log("No pictures to display to client.");
-          res.render('404');
-        }
-      });
     });
-  }
+  });
 });
+
+passport.authenticate('basic', { session: false });
 
 app.get('/gallery/:id/edit', function (req, res) {
   Picture.findAll( {where: { id: parseInt(req.params.id)}} )
@@ -79,35 +92,34 @@ app.get('/gallery/:id/edit', function (req, res) {
 });
 
 app.post('/gallery', function (req, res) {
-    var duplicate = false;
-    Picture.findOne({
-      where: {
-        title: req.body.title
-      }
-    }).then(function (picture) {
-      if(picture) {
-        console.log("setting duplicate as true.");
-        res.send("Picture already exists.");
-      }
-      else {
-        Picture.create({
-        title: req.body.title,
-        author: req.body.author,
-        url: req.body.url,
-        description: req.body.description,
-        })
-          .then(function (picture) {
-            res.render('success');
-          }).catch(function (error) {
-            console.log(error);
-            res.send("Unable to add picture.");
-          });
-      }
-    });
+  var duplicate = false;
+  Picture.findOne({
+    where: {
+      title: req.body.title
+    }
+  }).then(function (picture) {
+    if(picture) {
+      console.log("setting duplicate as true.");
+      res.send("Picture already exists.");
+    }
+    else {
+      Picture.create({
+      title: req.body.title,
+      author: req.body.author,
+      url: req.body.url,
+      description: req.body.description,
+      })
+        .then(function (picture) {
+          res.render('success');
+        }).catch(function (error) {
+          console.log(error);
+          res.send("Unable to add picture.");
+        });
+    }
+  });
 });
 
 app.put('/gallery/:id/edit', function (req, res) {
-  console.log("req.body: ", req.body);
   Picture.find( {
     where: {
       id: parseInt(req.params.id)
